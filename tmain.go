252f3e9d16e36c31a2cb388923e2b0d852fc4d40
jiebozeng <985199962@qq.com>
2022-03-24 20:55:13 +0800

    add timer

---
+++ tmain.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	tr "golangutils/timer"
+	"runtime"
+	"time"
+)
+
+func main() {
+	timer := tr.NewTimer(time.Second)
+	timer.Start()
+
+	// 1秒的定时器,执行10次
+	timer.AddTimer(time.Second, 10, func(*tr.Timer) {
+		fmt.Println("timer 1 second")
+	})
+
+	// 10秒的定时器,执行1次
+	timeid2 := timer.AddTimer(10*time.Second, 1, func(*tr.Timer) {
+		fmt.Println("timer 10 second")
+	})
+
+	timer.RemoveTimer(timeid2)
+
+	// 永久执行的1秒定时器任务
+	timer.AddTimer(time.Second, -1, func(*tr.Timer) {
+		fmt.Println("timer 1 second forever")
+	})
+	runtime.Goexit()
+}
