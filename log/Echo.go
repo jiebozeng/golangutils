c4ff84917a412b66f268113f3227ca7a05ca26da
jiebozeng <985199962@qq.com>
2022-03-24 17:13:53 +0800

    add Echo.go ,add print clearly tool

---
+++ log/Echo.go
@@ -0,0 +1,21 @@
+package log
+
+import "fmt"
+
+// println clearly
+func Println(args ...interface{}) {
+	fmt.Println("------------")
+	for _, v := range args {
+		fmt.Println(v)
+	}
+	fmt.Println("------------")
+}
+
+// print clearly
+func Print(args ...interface{}) {
+	fmt.Println("------------")
+	for _, v := range args {
+		fmt.Print(v," ")
+	}
+	fmt.Println("------------")
+}
