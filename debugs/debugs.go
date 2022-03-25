7b5c2000b2166069e83c3323ba5b7ab3f0c4ebf2
jiebozeng <985199962@qq.com>
2022-03-25 18:02:32 +0800

    add 8888 for debugs

--- debugs/debugs.go
+++ debugs/debugs.go
@@ -2,20 +2,20 @@ package debugs
 
 import "fmt"
 
-// println clearly 
+// println clearly
 func Println(args ...interface{}) {
-	fmt.Println("------------")
+	fmt.Println("------8888------")
 	for _, v := range args {
 		fmt.Println(v)
 	}
-	fmt.Println("------------")
+	fmt.Println("------8888------")
 }
 
 // print clearly not ln
 func Print(args ...interface{}) {
-	fmt.Println("------------")
+	fmt.Println("------8888------")
 	for _, v := range args {
 		fmt.Print(v, " ")
 	}
-	fmt.Println("------------")
+	fmt.Println("------8888------")
 }
