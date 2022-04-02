66cfeab22b3c8bac3d17ad984907a7b6b73b5e3b
jiebozeng <985199962@qq.com>
2022-04-02 17:37:47 +0800

    nothing

--- go.mod
+++ go.mod
@@ -4,4 +4,5 @@ go 1.16
 
 require (
 	github.com/golang-module/carbon v1.5.5
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- tmain.go
+++ tmain.go
@@ -1,32 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"github.com/jiebozeng/golangutils/debugs"
-	tr "github.com/jiebozeng/golangutils/timer"
 	"runtime"
-	"time"
 )
 
 func main() {
-	timer := tr.NewTimer(time.Second)
-	timer.Start()
-	debugs.Println("888")
-	// 1秒的定时器,执行10次
-	timer.AddTimer(time.Second, 10, func(*tr.Timer) {
-		fmt.Println("timer 1 second")
-	})
-
-	// 10秒的定时器,执行1次
-	timeid2 := timer.AddTimer(10*time.Second, 1, func(*tr.Timer) {
-		fmt.Println("timer 10 second")
-	})
-
-	timer.RemoveTimer(timeid2)
-
-	// 永久执行的1秒定时器任务
-	timer.AddTimer(time.Second, -1, func(*tr.Timer) {
-		fmt.Println("timer 1 second forever")
-	})
+	debugs.Print("test")
 	runtime.Goexit()
 }
