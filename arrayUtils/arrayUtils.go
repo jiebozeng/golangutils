796e66155df921535c8e8c217a215390a00ceb1a
jiebozeng <985199962@qq.com>
2022-05-23 20:20:04 +0800

    add matrix rotate

--- arrayUtils/arrayUtils.go
+++ arrayUtils/arrayUtils.go
@@ -5,10 +5,11 @@ import (
 	"reflect"
 	"time"
 )
-// add slice , array add slice 
+
+// add slice , array add slice
 func ItemExists(targetArray interface{}, item interface{}) bool {
 	arr := reflect.ValueOf(targetArray)
-	if arr.Kind() != reflect.Array && arr.Kind() != reflect.Slice{
+	if arr.Kind() != reflect.Array && arr.Kind() != reflect.Slice {
 		panic("targetArray Invalid data-type")
 	}
 	for i := 0; i < arr.Len(); i++ {
@@ -68,9 +69,9 @@ func ArrayDeleteString(arr []string, tar string) ([]string, bool) {
 }
 
 /**
- 指定元素在数组中存在个数
- return Array Exist item numbers
- */
+指定元素在数组中存在个数
+return Array Exist item numbers
+*/
 func ArrayExistsCount(arrayOrSlice interface{}, target interface{}) int32 {
 	count := int32(0)
 	targetValue := reflect.ValueOf(arrayOrSlice)
@@ -89,9 +90,10 @@ func ArrayExistsCount(arrayOrSlice interface{}, target interface{}) int32 {
 	}
 	return count
 }
+
 /*
 ArrayShuffle 打乱数组/切片排序.
- */
+*/
 func ArrayShuffle(arr interface{}) []interface{} {
 	val := reflect.ValueOf(arr)
 	typ := val.Kind()
@@ -112,4 +114,54 @@ func ArrayShuffle(arr interface{}) []interface{} {
 	})
 
 	return res
+}
+
+// change number to matrix i j
+func NumChangeToIJ(n int32, row int32, col int32) (int32, int32) {
+	return n / row, n % col
+}
+
+//Rotate the matrix 90 degrees counterclockwise
+func Rotate(matrix [][]int32) [][]int32 {
+	n := len(matrix)
+	for i := 0; i < n/2; i++ {
+		matrix[i], matrix[n-1-i] = matrix[n-1-i], matrix[i]
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < i; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+	return matrix
+}
+//Rotate the matrix 180 degrees counterclockwise
+func Rotate180(matrix [][]int32) [][]int32 {
+	n := len(matrix)
+	for i := 0; i < n/2; i++ {
+		matrix[i], matrix[n-1-i] = matrix[n-1-i], matrix[i]
+	}
+	for i := 0; i < n; i++ {
+		m := len(matrix[i])
+		for j := 0; j < m/2; j++ {
+			matrix[i][j], matrix[i][m-1-j] = matrix[i][m-1-j], matrix[i][j]
+		}
+	}
+	return matrix
+}
+////Rotate the matrix 270 degrees counterclockwise
+func Rotate270(matrix [][]int32) [][]int32 {
+	n := len(matrix)
+	for i := 0; i < n; i++ { // 次对角线翻转
+		m := len(matrix[i])
+		for j := 0; j < m-i; j++ {
+			matrix[i][j], matrix[n-j-1][m-i-1] = matrix[n-j-1][m-i-1], matrix[i][j]
+		}
+	}
+	for i := 0; i < n; i++ { // 每行按照中点翻转
+		m := len(matrix[i])
+		for j := 0; j < m/2; j++ {
+			matrix[i][j], matrix[i][m-j-1] = matrix[i][m-j-1], matrix[i][j]
+		}
+	}
+	return matrix
 }
