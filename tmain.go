ff8d3399c6b16b04123c5f33c029872bd14bfd39
unknown <985199962@qq.com>
2024-05-28 09:02:41 +0800

    delete un use things

--- tmain.go
+++ tmain.go
@@ -2,23 +2,19 @@ package main
 
 import (
 	"github.com/jiebozeng/golangutils/debugs"
-	"github.com/jiebozeng/golangutils/log"
-	"github.com/nats-io/nats.go"
-	"runtime"
 )
 
-/**
+/*
+*
 没啥用 不要看
 */
 func main() {
 	debugs.Print("test")
-	nc, err := nats.Connect(nats.DefaultURL)
-	if err != nil {
-		log.Error("connect nats error", err)
-	}
-
-	defer nc.Close()
-	runtime.Goexit()
+	//nc, err := nats.Connect(nats.DefaultURL)
+	//if err != nil {
+	//	log.Error("connect nats error", err)
+	//}
+	//
+	//defer nc.Close()
+	//runtime.Goexit()
 }
-
-
